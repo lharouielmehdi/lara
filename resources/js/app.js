da3abf417f0baac98fcc23fbdd6c37e93f479907
said wld lhwat <lharoui.elmehdi@gmail.com>
2020-05-07 05:55:52 +0200

    integrer le vusjs

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,9 +4,27 @@
  * building robust, powerful web applications using Vue and Laravel.
  */
 
-require('./bootstrap');
+require("./bootstrap");
 
-window.Vue = require('vue');
+window.Vue = require("vue");
+
+import VueRouter from "vue-router";
+
+Vue.use(VueRouter);
+
+let routes = [
+    {
+        path: "/Dashbor-d",
+        component: require("./components/Dashbor-d.vue").default
+    },
+    {
+        path: "/Pofile-User",
+        component: require("./components/Pofile-User.vue")
+    }
+];
+const router = new VueRouter({
+    routes // short for `routes: routes`
+});
 
 /**
  * The following block of code may be used to automatically register your
@@ -19,7 +37,10 @@ window.Vue = require('vue');
 // const files = require.context('./', true, /\.vue$/i);
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default));
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+Vue.component(
+    "example-component",
+    require("./components/ExampleComponent.vue")
+);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -28,5 +49,6 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
  */
 
 const app = new Vue({
-    el: '#app',
+    el: "#app",
+    router
 });
--- resources/js/components/Dashbor-d.vue
+++ resources/js/components/Dashbor-d.vue
@@ -3,7 +3,7 @@
     <div class="row justify-content-center">
       <div class="col-md-8">
         <div class="card">
-          <div class="card-header">hhhhhhhhhhhhh in dashbord</div>
+          <div class="card-header">helllo in dashbord</div>
 
           <div class="card-body">I'm an example component.</div>
         </div>
